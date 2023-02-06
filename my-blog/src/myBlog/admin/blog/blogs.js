0e15790a1a927f0738b68c2cc5d469450a013110
teal-oyunsuren17 <teal.2022.oyunsuren.purevsuren@gmail.com>
2023-02-06 17:37:31 +0800

    add at class

--- my-blog/src/myBlog/admin/blog/blogs.js
+++ my-blog/src/myBlog/admin/blog/blogs.js
@@ -4,7 +4,7 @@ import { AdminHeader } from "../adminHeader";
 
 export function Blogs() {
   const [blogs, setBlogs] = useState([]);
-  useEffect(() => {
+  function loadBlog() {
     axios.get("http://localhost:8000/blog").then((res) => {
       const { data, status } = res;
       if (status === 200) {
@@ -13,7 +13,21 @@ export function Blogs() {
         alert("Error");
       }
     });
+  }
+
+  useEffect(() => {
+    loadBlog();
   }, []);
+
+  function editBlog() {}
+
+  function deleteBlog() {
+    if (window.confirm("ustgah uu? ")) {
+      axios
+        .delete(`http://localhost:8000/blog/${blogs.id}`)
+        .then((res) => loadBlog());
+    }
+  }
   return (
     <>
       <AdminHeader />
@@ -22,8 +36,8 @@ export function Blogs() {
           <div key={blog.id} className={"d-flex flex-row gap-5"}>
             <p>{blog.id}</p>
             <p>{blog.title}</p>
-            <button>edit</button>
-            <button>delete</button>
+            <button onClick={editBlog}>edit</button>
+            <button onClick={deleteBlog}>delete</button>
           </div>
         ))}
       </div>
