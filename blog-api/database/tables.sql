7938788c2187c288d1170011d146b4e3b4f12707
teal-oyunsuren17 <teal.2022.oyunsuren.purevsuren@gmail.com>
2023-03-06 17:34:02 +0800

    add at class

--- blog-api/database/tables.sql
+++ blog-api/database/tables.sql
@@ -1,3 +1,7 @@
 create table blog (
     categoryId varchar(40) not null
 );
+
+select * from article left join category on article.category_id = category.id;
+
+select article.id , article.title, article.picture, article.text, article.category_id, category.name as category_name from article left join category on article.category_id = category.id;
